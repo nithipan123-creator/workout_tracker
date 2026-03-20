ebe69d8d6d810e61a79648b77b4dc67ccb560307
Nithipan Sivakanthan <nithipan123@gmail.com>
2026-03-20 11:58:38 -0400

    Update Flask app to run on configurable port
    Modify Flask app to use environment variable for port.

--- app.py
+++ app.py
@@ -133,6 +133,8 @@ def chart_data():
     
     return {'labels': labels, 'data': data}
 
-if __name__ == '__main__':
-    # Start the Flask development server on http://127.0.0.1:5000
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
